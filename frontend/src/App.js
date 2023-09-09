23c93dd6cb62d5c633b82fd7373499bf381fe839
Felsen Gestão e Tecnologia <lucas.rocha@felsen.ltda>
2023-09-09 03:26:28 -0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,48 +6,60 @@ import { createTheme, ThemeProvider } from "@material-ui/core/styles";
 import { ptBR } from "@material-ui/core/locale";
 
 const App = () => {
-  const [locale, setLocale] = useState();
+	const [locale, setLocale] = useState();
 
-  const theme = createTheme({
-    palette: {
-      primary: {
-        main: "#2196F3",
-      },
-      type: "dark", // Configuração para o tema dark
-    },
-    overrides: {
-      MuiCssBaseline: {
-        "@global": {
-          scrollbarStyles: {
-            "&::-webkit-scrollbar": {
-              width: "8px",
-              height: "8px",
-            },
-            "&::-webkit-scrollbar-thumb": {
-              boxShadow: "inset 0 0 6px rgba(170, 0, 126, 126)",
-              backgroundColor: "#2B5A80",
-            },
-          },
+  const theme = createTheme(
+    {
+      scrollbarStyles: {
+        '&::-webkit-scrollbar': {
+          width: '8px',
+          height: '8px',
+        },
+        '&::-webkit-scrollbar-thumb': {
+          boxShadow: 'inset 0 0 6px rgba(170, 0, 126, 126)',
+          backgroundColor: '#2B5A80',
         },
       },
-    },
-  });
+      palette: {
+        primary: { main: "#110142" },
+        third: { main: "#E75A4D" }
+      },
 
-  useEffect(() => {
-    const i18nlocale = localStorage.getItem("i18nextLng");
-    const browserLocale =
-      i18nlocale.substring(0, 2) + i18nlocale.substring(3, 5);
+      barraSuperior: {
+        primary: { main: "linear-gradient(to right, #E75A4D 100%, #E75A4D 26%, #E75A4D 100%)" },
+        secondary: { main: "#ffffff" },
+      },
 
-    if (browserLocale === "ptBR") {
-      setLocale(ptBR);
-    }
-  }, []);
+      barraLateral: {
+        primary: { main: "#F7F6F5" },
+      },
 
-  return (
-    <ThemeProvider theme={theme}>
-      <Routes />
-    </ThemeProvider>
+      icons: {
+        primary: { main: "#E75A4D" }
+      },
+      textColorMenu: {
+        primary: { main: "#FFFFFF" },
+        secondary: { main: "#FFFFFF" }
+      
+      },    },
+    locale
   );
+
+	useEffect(() => {
+		const i18nlocale = localStorage.getItem("i18nextLng");
+		const browserLocale =
+			i18nlocale.substring(0, 2) + i18nlocale.substring(3, 5);
+
+		if (browserLocale === "ptBR") {
+			setLocale(ptBR);
+		}
+	}, []);
+
+	return (
+		<ThemeProvider theme={theme}>
+			<Routes />
+		</ThemeProvider>
+	);
 };
 
 export default App;
