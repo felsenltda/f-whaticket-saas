ebfe272d5c8397bae463aabadbf2de279db5f746
Felsen Gestão e Tecnologia <lucas.rocha@felsen.ltda>
2023-09-09 01:16:52 -0300

    update_all_2
    all 2

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -2,12 +2,20 @@ import React, { useState, useContext } from "react";
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import TextField from "@material-ui/core/TextField";
-import Box from "@material-ui/core/Box";
 import Typography from "@material-ui/core/Typography";
-import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles, createTheme, ThemeProvider } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
 import { AuthContext } from "../../context/Auth/AuthContext";
 import logo from "../../assets/zapsimples.png";
+
+const theme = createTheme({
+  palette: {
+    type: "dark", // Tema escuro
+    primary: {
+      main: "#2196F3", // Cor primária (azul)
+    },
+  },
+});
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -15,14 +23,13 @@ const useStyles = makeStyles((theme) => ({
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
-    backgroundColor: "white", // Alterado para branco
-    padding: theme.spacing(3), // Adicionado padding para espaçamento interno
-    borderRadius: "8px", // Adicionado bordas arredondadas
-    boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)", // Adicionado sombra
+    padding: theme.spacing(4), // Aumentando o espaçamento interno
+    borderRadius: "8px",
+    boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)",
   },
   avatar: {
     margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
+    backgroundColor: theme.palette.primary.main,
   },
   form: {
     width: "100%",
@@ -50,59 +57,60 @@ const Login = () => {
   };
 
   return (
-    <Container component="main" maxWidth="xs">
+    <ThemeProvider theme={theme}>
       <CssBaseline />
-      <div className={classes.paper}>
-        <div>
-          <img
-            style={{ margin: "0 auto", height: "80px", width: "100%" }}
-            src={logo}
-            alt="Whats"
-          />
+      <Container component="main" maxWidth="xs">
+        <div className={classes.paper}>
+          <div>
+            <img
+              style={{ margin: "0 auto", height: "80px", width: "100%" }}
+              src={logo}
+              alt="Whats"
+            />
+          </div>
+          <Typography component="h1" variant="h5">
+            Faça o login
+          </Typography>
+          <form className={classes.form} noValidate onSubmit={handleSubmit}>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email"
+              name="email"
+              value={user.email}
+              onChange={handleChangeInput}
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Senha"
+              type="password"
+              id="password"
+              value={user.password}
+              onChange={handleChangeInput}
+              autoComplete="current-password"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              Entrar
+            </Button>
+          </form>
         </div>
-        <Typography component="h1" variant="h5">
-          Faça o login
-        </Typography>
-        <form className={classes.form} noValidate onSubmit={handleSubmit}>
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="email"
-            label="Email"
-            name="email"
-            value={user.email}
-            onChange={handleChangeInput}
-            autoComplete="email"
-            autoFocus
-          />
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Senha"
-            type="password"
-            id="password"
-            value={user.password}
-            onChange={handleChangeInput}
-            autoComplete="current-password"
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-          >
-            Entrar
-          </Button>
-        </form>
-      </div>
-      <Box mt={8}></Box>
-    </Container>
+      </Container>
+    </ThemeProvider>
   );
 };
 
