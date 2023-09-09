c6255b6a7092f5d691255f2928ae53dbd4a24b5c
Felsen Gestão e Tecnologia <lucas.rocha@felsen.ltda>
2023-09-09 02:52:37 -0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,60 +6,61 @@ import { createTheme, ThemeProvider } from "@material-ui/core/styles";
 import { ptBR } from "@material-ui/core/locale";
 
 const App = () => {
-	const [locale, setLocale] = useState();
+  const [locale, setLocale] = useState();
 
   const theme = createTheme(
     {
       scrollbarStyles: {
-        '&::-webkit-scrollbar': {
-          width: '8px',
-          height: '8px',
+        "&::-webkit-scrollbar": {
+          width: "8px",
+          height: "8px",
         },
-        '&::-webkit-scrollbar-thumb': {
-          boxShadow: 'inset 0 0 6px rgba(170, 0, 126, 126)',
-          backgroundColor: '#2B5A80',
+        "&::-webkit-scrollbar-thumb": {
+          boxShadow: "inset 0 0 6px rgba(170, 0, 126, 126)",
+          backgroundColor: "#2B5A80",
         },
       },
       palette: {
-        primary: { main: "#110142" },
-        third: { main: "#E75A4D" }
+        type: "dark", // Definir o tipo de paleta como escuro (dark)
+        primary: { main: "#E75A4D" }, // Cor primária (vermelho)
+        secondary: { main: "#110142" }, // Cor secundária (roxo)
       },
-
       barraSuperior: {
-        primary: { main: "linear-gradient(to right, #E75A4D 100%, #E75A4D 26%, #E75A4D 100%)" },
-        secondary: { main: "#ffffff" },
+        primary: {
+          main:
+            "linear-gradient(to right, #E75A4D 100%, #E75A4D 26%, #E75A4D 100%)",
+        },
+        secondary: { main: "#ffffff" }, // Cor da barra superior (branco)
       },
-
       barraLateral: {
-        primary: { main: "#F7F6F5" },
+        primary: { main: "#1A1A1A" }, // Cor da barra lateral (cinza escuro)
       },
-
       icons: {
-        primary: { main: "#E75A4D" }
+        primary: { main: "#FFFFFF" }, // Cor dos ícones (branco)
       },
       textColorMenu: {
-        primary: { main: "#FFFFFF" },
-        secondary: { main: "#FFFFFF" }
-      
-      },    },
+        primary: { main: "#FFFFFF" }, // Cor do texto do menu (branco)
+        secondary: { main: "#FFFFFF" }, // Cor secundária do texto do menu (branco)
+      },
+    },
     locale
   );
 
-	useEffect(() => {
-		const i18nlocale = localStorage.getItem("i18nextLng");
-		const browserLocale =
-			i18nlocale.substring(0, 2) + i18nlocale.substring(3, 5);
+  useEffect(() => {
+    const i18nlocale = localStorage.getItem("i18nextLng");
+    const browserLocale =
+      i18nlocale.substring(0, 2) + i18nlocale.substring(3, 5);
 
-		if (browserLocale === "ptBR") {
-			setLocale(ptBR);
-		}
-	}, []);
+    if (browserLocale === "ptBR") {
+      setLocale(ptBR);
+    }
+  }, []);
 
-	return (
-		<ThemeProvider theme={theme}>
-			<Routes />
-		</ThemeProvider>
-	);
+  return (
+    <ThemeProvider theme={theme}>
+      <Routes />
+    </ThemeProvider>
+  );
 };
 
 export default App;
